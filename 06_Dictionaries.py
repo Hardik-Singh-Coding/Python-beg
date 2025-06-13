559f6fa02a8a45107c93e3eafb7e0fe99c94de14
Hardik-Singh-Coding <Hardik3003Singh@gmail.com>
2025-06-13 18:30:12 +0530

    Added dictionaries

---
+++ 06_Dictionaries.py
@@ -0,0 +1,24 @@
+person = { "name": "Jimmy", "age": 19, "fav_color": "grey"}
+
+person["name"] = "John"
+ 
+# print(person.get("fav_color", "green"))
+
+# print(person.pop("name"))
+# print(person)
+# print(person.popitem())
+# print(person)
+
+# print(person.keys())
+# print(list(person.keys()))
+# print(list(person.values()))
+
+# print(list(person.items()))
+
+# print(len(person))
+
+# person_copy = person.copy()
+# print(person_copy)
+
+# del person['age']
+print(person['name'])
