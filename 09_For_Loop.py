29b7338b4222d2a28c56e49e3553c8570da9ccd1
Hardik-Singh-Coding <Hardik3003Singh@gmail.com>
2025-06-16 18:28:04 +0530

    Added For loop

---
+++ 09_For_Loop.py
@@ -0,0 +1,19 @@
+name = "Abhishek"
+
+list1 = [39, "Rohan", True, "Gradient descent"]
+
+dictionary = {"group1":"Hardik, Rohan, Vansh", "group2":"Abhishek, Rohit, Karen"}
+
+# for keys in dictionary:
+#     print(keys) # To print the keys 
+
+# for values in dictionary:
+#     print(dictionary[values]) # To print the values of the keys
+
+# for i in range(0,7,2): # (start, stop, step)
+#     print(i)
+
+for element in list1:
+    if element == True:
+        break
+    print(element)
