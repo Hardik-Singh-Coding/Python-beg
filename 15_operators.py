7c3b3b98edb947fc740a194afad596d8049b51d7
Hardik-Singh-Coding <Hardik3003Singh@gmail.com>
2025-07-09 23:40:12 +0530

    Learnt about operators

---
+++ 15_operators.py
@@ -0,0 +1,30 @@
+# Floor division divides and rounds off to the nearest integer
+a = 5
+b = 2
+print(a // b)
+
+
+# Bitwise opeartors 
+x = 5
+print(+x)
+print(-x)
+print(~x)
+
+
+# Identity operators
+p = 20
+q = 20
+print(p is q)
+print(p is not q)
+
+
+# Membership operator
+element1 = "Summer"
+part = "mmer"
+print(part in element1)
+print(part not in element1)
+
+
+
+# If two or more ***binary opearators*** have same precedence then the expression is evaluated from left to right. 
+# If two or more ***assignment and/or exponentiation operator*** have the same precedence then the expression is evaluated from right to left. 
