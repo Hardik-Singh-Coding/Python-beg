4ff5e48deb37eef2eaf9c14d2240bd4d2dcf2af0
Hardik-Singh-Coding <Hardik3003Singh@gmail.com>
2025-07-09 23:38:45 +0530

    Learnt about variables

---
+++ 11_Global_and_local_variables.py
@@ -0,0 +1,40 @@
+# Example 1
+# x = "awesome"
+
+# def func_1():
+#     x = "fantastic"
+#     print("I am", x)
+
+# func_1()
+# print("I am", x)
+
+
+
+
+# Example 2 
+# def func_2():
+#     global a
+#     a = "Mango"
+
+# func_2()
+# print("My favourite fruit is " + a)
+
+
+
+
+
+# Example 3
+# q = 50
+
+# def luckyfunc():
+#     global q
+#     q = 45
+
+# luckyfunc()
+# print(q, "is my lucky number")
+
+
+
+# To check if the variable is of a specific type
+var = 30
+print(isinstance(var, int))
