f4c704514a0b2b3ce02bc4073ae61c5bf3614e19
Hardik-Singh-Coding <Hardik3003Singh@gmail.com>
2025-07-11 11:59:35 +0530

    Updated Match case

--- 08_Match_case_statetments.py
+++ 08_Match_case_statetments.py
@@ -6,3 +6,15 @@ match x:
     case 1: print("x is one")
 
     case _: print(f"Number is {x}")
+
+
+
+day = 4
+month = 5
+match day:
+    case 1|2|3|4|5 if month == 5:
+        print("It's a weekday in May")
+    case 1|2|3|4|5 if month == 4:
+        print("It's a weekday in April")
+    case _:
+        print("Error")
