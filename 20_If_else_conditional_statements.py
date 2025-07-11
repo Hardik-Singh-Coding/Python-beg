9c633056e075d114f71a6c16e52e684d2d68805b
Hardik-Singh-Coding <Hardik3003Singh@gmail.com>
2025-07-11 11:05:42 +0530

    Added control statement

---
+++ 20_If_else_conditional_statements.py
@@ -0,0 +1,12 @@
+# short-hand if 
+if 30>20: print("30 is greater than 20")
+
+
+
+# short had if-else ( ternary operator )
+print("20 is greater than 30") if 20>30 else print("30 is greater than 20")
+
+
+
+# Multiple else statements on the same line
+print("20 is greater than 30") if 20>=30 else print("30 and 30 are equal") if 30==30 else print("30 is greater than 20")
